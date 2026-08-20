1d94193296e562c3bd5cfca61d2fd025cf89a967
Santiagortegab <santiago.2005bur@gmail.com>
2026-08-20 16:33:58 -0600

    VDA en backround para optimizar tiempo de respuesta

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from os import read
+import threading
+import time
 import cv2
 import torch
 import numpy as np
@@ -25,71 +28,74 @@ depth_model = depth_model.to(DEVICE).eval()
 
 print("Modelo YOLOv8 y Depth Anything cargados correctamente.")
 
+frame_actual = None
+ultimo_mapa = None
+lock= threading.Lock()
 
-cap = cv2.VideoCapture("example2.mp4")
+def thread_profundidad():
+    global frame_actual, ultimo_mapa
+    while True:
+        frame_process = None
+        with lock:
+            if frame_actual is not None:
+                frame_process = frame_actual.copy()
+        if frame_process is None:
+            time.sleep(0.01)
+            continue
+        frame_rgb = cv2.cvtColor(frame_process, cv2.COLOR_BGR2RGB)
+        with torch.no_grad():
+            mapa = depth_model.infer_video_depth_one(
+                frame_rgb,
+                input_size=250,
+                device=DEVICE,
+                fp32=True)
+        with lock:
+            ultimo_mapa = mapa.copy()
+
+bg_thread = threading.Thread(target=thread_profundidad, daemon=True)
+bg_thread.start()
+print("Procesamiento de profundidad listo")
+
+cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("Error al abrir el archivo de video.")
     exit()
 
-contador_frames = 0
-salto_frames = 8
-resolucion_vda = (350)
-
-
-while True:
+while cap.isOpened():
     ret, frame = cap.read()
-
     if not ret:
-        print("Fin del video.")
+        print("Fin del video")
         break
 
-    contador_frames += 1
+    frame_process = cv2.resize(frame, (854, 480))
 
-    if contador_frames % salto_frames != 0:
-        continue
+    with lock:
+        frame_actual = frame_process.copy()
 
-    frame = cv2.resize(frame, (854,480))
+    resultados_yolo = detector_yolo(frame_process, classes=[0], verbose=False)
+    mapa_actual = None
+    with lock:
+        if ultimo_mapa is not None:
+            mapa_actual = ultimo_mapa.copy()
 
-    resultados = detector_yolo(frame, classes=[0], verbose=False)
-
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    with torch.no_grad():
-        profundidad = depth_model.infer_video_depth_one(
-            frame_rgb,
-            input_size=resolucion_vda,
-            device=DEVICE,
-            fp32=True
-        )
-
-    mapa_norm = cv2.normalize(profundidad, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
-    mapa_color = cv2.applyColorMap(mapa_norm, cv2.COLORMAP_INFERNO)
-
-    for resultado in resultados:
-        cajas = resultado.boxes
-        for caja in cajas:
-
+    for resultado in resultados_yolo:
+        for caja in resultado.boxes:
             x1, y1, x2, y2 = caja.xyxy[0].int().tolist()
-            region_profundidad_humano = mapa_norm[y1:y2, x1:x2]
-            if region_profundidad_humano.size > 0:
-                profundidad_estimada = np.median(region_profundidad_humano)
-            else:
-                profundidad_estimada = 0
 
-            confianza = caja.conf[0].item()
-            cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
-            label_conf = f"Operador: {confianza:.2f}"
-            label_prof = f"Profundidad: {profundidad_estimada:.2f}"
-            cv2.putText(frame, label_prof, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            
+            distancia_label = "Calculando profundidad"
+            if mapa_actual is not None:
+                region = mapa_actual[y1:y2, x1:x2]
+                if region.size > 0:
+                    distancia_profundidad = np.max(region)
+                    distancia_label = f"Profundidad:{distancia_profundidad}"
+            cv2.rectangle(frame_process, (x1,y1), (x2,y2), (0, 255, 0), 2)
+            cv2.putText(frame_process, distancia_label, (x1, y1+100), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 2)
+    cv2.imshow("Sistema Seguridad 3D", frame_process)
 
-    cv2.imshow("Sistema de seguridad 3D - Deteccion RGB", frame)
-    cv2.imshow("Sistema de seguridad 3D - Mapa de profundidad", mapa_color)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
